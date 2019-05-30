bf412ef7bfec7e18e2900a055ab725ad2fce6b0d
micamello <42096699+micamello@users.noreply.github.com>
2019-05-30 09:22:21 -0500

    Add files via upload

---
+++ includes/mpdf/ttfontdata/gothic.mtx.php
@@ -0,0 +1,26 @@
+<?php
+$name='CenturyGothic';
+$type='TTF';
+$desc=array (
+  'Ascent' => 1006.0,
+  'Descent' => -220.0,
+  'CapHeight' => 1006.0,
+  'Flags' => 4,
+  'FontBBox' => '[-169 -220 1152 971]',
+  'ItalicAngle' => 0.0,
+  'StemV' => 87.0,
+  'MissingWidth' => 750.0,
+);
+$up=-96;
+$ut=58;
+$ttffile='C:/wamp64/www/admincuestionario/includes/mpdf/ttfonts/GOTHIC.TTF';
+$TTCfontID='0';
+$originalsize=61280;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='gothic';
+$panose=' 2 4 2 b 5 2 2 2 2 2 2 4';
+$haskerninfo=false;
+$unAGlyphs=false;
+?>
